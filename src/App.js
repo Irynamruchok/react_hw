2a4b5e6c8233ee0387809bae57922be09ddfc108
Irynamruchok <mruchokira@gmail.com>
2023-08-03 17:01:43 +0300

    hw8

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TestUseMemo from "./Components/TestUseMemo/TestUseMemo";
+import {useState} from "react";
+import useToggle from "./Components/Hooks/UseToggle/useToggle";
+import UserComponent from "./Components/UserComponent/UserComponent";
+import ArrayComponent from "./Components/ArrayComponent/ArrayComponent";
 
 function App() {
+  const [data, setData] = useState(1)
+  const [number, setNumber] = useState(1)
+  const [isDarkMode,toggleDarkMode] = useToggle(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width:'100%', height:'100%', display:'flex',justifyContent:'space-between', flexDirection:'column',backgroundColor: isDarkMode? '#333' : '#fff', padding:'50px' }}>
+        <div>
+            <h3>{isDarkMode? 'Dark Mode' : 'Light Mode'}</h3>
+            <button onClick={toggleDarkMode}>Change Background Color </button>
+        </div>
+<div>
+      <TestUseMemo data={data} number={number} setData={setData} setNumber={setNumber}/>
+</div>
+        <div>
+            <UserComponent/>
+        </div>
+        <div> <ArrayComponent/> </div>
     </div>
   );
 }
